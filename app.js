23601f6441106edaaaef943ee0bcd123e647754d
kuzudoli <enescakkr@windowslive.com>
2022-02-25 14:41:22 +0300

    Views and partials created

--- app.js
+++ app.js
@@ -2,14 +2,28 @@ const express = require("express");
 const app = express();
 const port = 3278;
 
+const ejs = require("ejs");
+
+//Template Engine
+app.set("view engine","ejs");
+//Middlewares
+app.use(express.static("public"));
+
 app.get('/',(req,res)=>{
-    const blog = { 
-        id: 1, 
-        title: "Blog title",
-        description: "Blog description" 
-    }
-    res.send(blog);
+    res.render("index");
 });
+
+app.get('/about',(req,res)=>{
+    res.render("about");
+});
+
+app.get('/add_post',(req,res)=>{
+    res.render("add_post");
+});
+
+// app.get('/post',(req,res)=>{
+//     res.render("post");
+// });
 
 app.listen(port,()=>{
     console.log(`${port} Listening...`);
